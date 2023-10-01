d3b7f374feb84bd5124e92175e5d81d99c1a3a28
binineni <sook7205@naver.com>
2023-10-01 23:19:48 +0900

    기업요약 분석 서비스 오류 수정중

--- streamlit_mockup/추천공모주.py
+++ streamlit_mockup/추천공모주.py
@@ -491,8 +491,9 @@ if choose == "진행 예정 청약":
             st.caption('기업별 투자설명서를 요약한 정보입니다.')
             st.text('')
 
+            st.text('comp_analysis')
             st.header(comp_analysis['종목코드'])
-            st.header('')
+            st.text('df_pred_i')
             st.header(df_pred['종목코드'][i])
 
             #df의 i번째 행과 종목코드를 비교하여 comp에서 인덱싱
